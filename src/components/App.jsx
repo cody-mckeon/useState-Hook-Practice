6b72b0cc9ed1a780d824127bd9e30db09cf55dc8
cody-mckeon <codymckeon@gmail.com>
2021-06-08 17:14:22 -0700

    setInterval using Time

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+//Challenge:
+//1. Given that you can get the current time using:
+let time = new Date().toLocaleTimeString();
+console.log(time);
+//Show the latest time in the <h1> when the Get Time button
+//is pressed.
+
+//2. Given that you can get code to be called every second
+//using the setInterval method.
+//Can you get the time in your <h1> to update every second?
+
+//e.g. uncomment the code below to see Hey printed every second.
+// function sayHi() {
+//   console.log("Hey");
+// }
+// setInterval(sayHi, 1000);
 
 function App() {
+  setInterval(displayTime, 1000);
+  let now = new Date().toLocaleTimeString();
+  console.log(now);
+
+  const [time, setTime] = useState(now);
+
+  function displayTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={displayTime}>Get Time</button>
     </div>
   );
 }
